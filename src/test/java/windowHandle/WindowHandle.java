468642ceeb9e53267479c9e71156bc258b758f27
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-07-19 17:06:17 +0530

    WindowHandle.java updated

--- src/test/java/windowHandle/WindowHandle.java
+++ src/test/java/windowHandle/WindowHandle.java
@@ -29,6 +29,28 @@ public class WindowHandle {
         System.out.println(driver.getTitle());
         driver.switchTo().window(parentWindow);
         System.out.println(driver.getTitle());
+    }
+
+    @Test
+    public void openNewSeperateWindow() {
+        WebDriver driver = WebDriverManager.chromedriver().create();
+        driver.manage().window().maximize();
+        driver.get("https://demo.automationtesting.in/Windows.html");
+        driver.findElement(By.xpath("//a[text()='Open New Seperate Windows']")).click();
+        System.out.println(driver.getTitle());
+        String parentWindow = driver.getWindowHandle();
+        driver.findElement(By.xpath("//button[text()='click']")).click();
+        System.out.println(driver.getTitle());
+        Set<String> windowHandles = driver.getWindowHandles();
+        for (String window : windowHandles) {
+            if (!window.equalsIgnoreCase(parentWindow)) {
+                driver.switchTo().window(window);
+                break;
+            }
+        }
+        System.out.println(driver.getTitle());
+        driver.switchTo().window(parentWindow);
+        System.out.println(driver.getTitle());
 
 
     }
